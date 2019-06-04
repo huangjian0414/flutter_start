8d3b238c34e2c7be1b9e3ced582e7209a9e4f9e5
huangjian <huangjian@lanyife.com>
2019-06-04 18:39:53 +0800

    登录界面优化，textfield 处理

--- flutter_app/lib/Main/LoginPage.dart
+++ flutter_app/lib/Main/LoginPage.dart
@@ -12,25 +12,50 @@ class LoginPage extends StatefulWidget{
 }
 
 class LoginPageState extends State<LoginPage> {
+
+  final _nameControl = TextEditingController();
+  final _pwdControl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return CupertinoPageScaffold(
+
+    return Scaffold(
       backgroundColor: Color(0xFF63BBF7),
-      child:
-        Stack(children: <Widget>[
-          Positioned(child: Text('000000',textAlign: TextAlign.center,),left: 0,right: 0,top: 100),
-          Positioned(child: Text('1234566'),left: 0,right: 0,top: 300),
-          Positioned(child: Image(image: AssetImage('Images/登录背景.png')),left: 0,right: 0,bottom: 0,),
-          Column(
-            children: <Widget>[
-              Container(
-                child: Text('hkbksgak'), margin: EdgeInsets.only(top: 0),),
-              Container(child: Text('hk999999'),
-                margin: EdgeInsets.only(top: 200),
-                alignment: Alignment.bottomCenter,)
-            ],)
-        ],)
+      body:
+      Stack(children: <Widget>[
+        Positioned(
+          child: Image(image: AssetImage('Images/登录背景.png')),
+          left: 0,
+          right: 0,
+          bottom: 0,),
+        Column(children: <Widget>[Container(child: Image(
+          image: AssetImage('Images/公司LOGO.png'),
+          width: 185,
+          height: 25,),
+          margin: EdgeInsets.only(top: 142),
+          alignment: Alignment.topCenter,),
+          Container(child: TextField(controller: _nameControl,
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.phone, color: Color(0xFFFFFFFF),),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFFFFFFF))),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFFFFFFF)))),),
+            margin: EdgeInsets.only(left: 36, top: 60, right: 36),),
+          Container(child: TextField(controller: _pwdControl,
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.print, color: Color(0xFFFFFFFF),),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFFFFFFF))),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFFFFFFF)))),),
+            margin: EdgeInsets.only(left: 36, top: 20, right: 36),)
+
+        ],),
+
+
+      ],)
       ,);
   }
 }
